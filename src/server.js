ec546446ec57a3237a39b4d6fc2b4ea041486f52
Eduardo Ramos <eduardosram10@gmail.com>
2023-12-30 18:50:32 -0300

    Streams no Node.js - Entendendo Buffers no Node.js

--- src/server.js
+++ src/server.js
@@ -2,27 +2,41 @@ import http from 'http';
 
 const users = []
 
-const server = http.createServer((req, res) => {
-  const {method, url} = req;
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
 
-  if(method === 'GET' && url === '/users'){
-    return res
-    .setHeader('Content-Type', 'application/json')
-    .end(JSON.stringify(users));
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
   }
 
-  if(method === 'POST' && url === '/users'){
+try {
+  req.body = JSON.parse(Buffer.concat(buffers).toString())
+} catch {
+  req.body = null
+}
+
+  if (method === 'GET' && url === '/users') {
+    return res
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(users));
+  }
+
+  if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body;
+
     users.push({
       id: 1,
-      name: 'John Doe',
-      email: 'johndoe@example.com'
+      name,
+      email
     })
 
-    return res.end('Criação de Usuarios');
+    return res.end('Criação de Usuarios - nome:' + name + ', email:' + email + '.👌');
   }
 
   return res.end('Hello, IGNITE!');
 })
 
 server.listen(3333);
-
+console.log("Servidor Rodando na porta: 3333🚀")
