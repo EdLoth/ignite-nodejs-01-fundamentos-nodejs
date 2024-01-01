393f03065e5781c3ee0cd276d2a1cb47baa91d87
Eduardo Ramos <eduardosram10@gmail.com>
2024-01-01 15:56:48 -0300

    Rotas da aplicação - Filtrando lista do banco de dados

--- src/database.js
+++ src/database.js
@@ -19,12 +19,18 @@ export class Database {
     fs.writeFile(databasePath, JSON.stringify(this.#database))
   }
 
-  select(table){
-    const data = this.#database[table] ?? []
+  select(table, search) {
+    let data = this.#database[table] ?? []
 
-    return data;
+    if (search) {
+      data = data.filter(row => {
+        return Object.entries(search).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase())
+        })
+      })
+    }
   }
-
+  
   insert(table, data){
     if(Array.isArray(this.#database[table])){
       this.#database[table].push(data)
